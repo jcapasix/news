4d7ca5c9773a740bcfbf11c68b79cf09ad224521
Jordan Capa <jcapasix@gmail.com>
2022-07-11 22:50:25 -0500

    Added project settings

---
+++ Data/Data/Data.h
@@ -0,0 +1,18 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Jordan Capa on 11-07-22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
